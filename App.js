a3d0df9f6fa2e77aa92e79a26170b79afbbfa680
Chocatrip <alejandro.diaz@mail.udp.cl>
2021-03-17 17:24:43 -0300

    Se agrega carpeta components

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {styles} from './styles';
-import { Text, SafeAreaView, Button, Alert, TextInput } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { styles } from "./styles";
+import { Text, SafeAreaView, Button, TextInput } from "react-native";
 
 export default function App() {
   const [number, onChangeNumber] = React.useState(null);
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hola mundo!</Text>
-      <Button
-        title="Press me"
-        onPress={() => alert('Boton presionado')}
-      />
+      <Text>Hola mundo! kk</Text>
+      <Button title="Press me" onPress={() => alert("El botón se presionó")} />
       <TextInput
-      
         style={styles.input}
-        onChangeText={number => onChangeNumber(number)}
+        onChangeText={(number) => onChangeNumber(number)}
         value={number}
         placeholder="number"
         keyboardType="numeric"
-      
       />
       <StatusBar style="auto" />
     </SafeAreaView>
