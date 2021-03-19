a75030632273f589c10a68edb3c0a4233383d015
Chocatrip <alejandro.diaz@mail.udp.cl>
2021-03-19 00:18:46 -0300

    Se borran los componentes y se agregan a inicio.js para poder hacer uso de las variables textInput

--- screens/Inicio.js
+++ screens/Inicio.js
@@ -1,21 +1,37 @@
-import React from "react";
-import { Text, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
-
-import InputCredito from "../components/Texto Credito";
-import InputValor from "../components/Texto Cuota Mensual";
-import InputCuotas from "../components/Texto N Cuotas";
+import React,{useState} from "react";
+import { Text, SafeAreaView, StyleSheet, TouchableOpacity, TextInput,AsyncStorage } from "react-native";
 
 function Inicio() {
   //Calcular CAE
+  const [credito,setCredito] = useState('');
+  const [cuota,setCuota] = useState('');
+  const [nCuotas,setNcuotas] = useState('');
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.bigTitle}>Calcular Cae</Text>
+
+
       <Text style={styles.title}>Ingrese Credito</Text>
-      <InputCredito />
+      <TextInput
+        style={styles.input}
+        placeholder="Precio al contado"
+        onChangeText={(val) => setCredito(val)}
+      />
+
       <Text style={styles.title}>Ingrese Valor de Cuota</Text>
-      <InputValor />
+      <TextInput
+        style={styles.input}
+        placeholder="Valor de la cuota mensual"
+        onChangeText={(val) => setCuota(val)}
+      />    
+      
       <Text style={styles.title}>Ingrese Número de Cuotas</Text>
-      <InputCuotas />
+      <TextInput
+        style={styles.input}
+        placeholder="Numero de cuotas"
+        onChangeText={(val) => setNcuotas(val)}
+      />
 
       <TouchableOpacity
         onPress={() => Alert.alert("CAE calculado")}
@@ -29,9 +45,43 @@ function Inicio() {
 
 export default Inicio;
 
+
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  title: { fontSize: 25 },
-  bigTitle: { fontSize: 40, marginBottom: 50 },
-  button: { backgroundColor: "tomato", padding: 15, marginTop: 10 },
+  container: { 
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center", 
+  },
+
+  title: { 
+    fontSize: 30,
+    borderBottomColor: "#ff4d4d",
+    textShadowRadius : 3,
+   },
+
+  bigTitle: { 
+    fontSize: 50, 
+    marginBottom: 20,
+    marginTop : -60,
+    textShadowRadius: 5, 
+  },
+
+  button: { 
+    backgroundColor: "#ff4d4d", 
+    padding: 25,
+    marginTop: 10,
+    borderRadius :50,
+  },
+
+  input:{
+    height: 60,
+    margin: 12,
+    padding :20,
+    paddingHorizontal:40,
+    borderWidth: 2,
+    borderColor :'#ff4d4d',
+    textAlign : 'center',
+    borderRadius : 30,
+    fontSize: 15,
+  },   
 });
