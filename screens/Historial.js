1b0e68b8185e52bb8d527ac171d32640a1a60542
frankbydefault <franco.hauva@gmail.com>
2021-03-19 13:34:26 -0300

    Actualizo el stilo de historial.js

--- screens/Historial.js
+++ screens/Historial.js
@@ -1,13 +1,94 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e7772",
+    title: "Forth Item",
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+function display() {
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+}
 
 function Historial() {
-  //Historial
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Historial</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Historial"
+        component={display}
+        options={{
+          title: "Historial",
+          headerStyle: {
+            backgroundColor: "#444",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerTitleStyle: { alignSelf: "center" },
+        }}
+      />
+    </Stack.Navigator>
   );
 }
 
 export default Historial;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#DFDFDF",
+    padding: 55,
+    marginVertical: 5,
+    marginHorizontal: 16,
+    borderWidth: 2,
+    borderColor: "tomato",
+    borderRadius: 40,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
