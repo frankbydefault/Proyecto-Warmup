543e8c9a9e543f995e0352af320fd91a4a969c94
frankbydefault <franco.hauva@gmail.com>
2021-03-24 21:52:10 -0300

    App final

--- calculoCae2.js
+++ calculoCae2.js
@@ -1,86 +1,63 @@
-var Datastore = require('react-native-local-mongodb');
+var Datastore = require("react-native-local-mongodb");
 export var db = new Datastore();
-function sumatoria(n, C, k){
-    
-    let result = 0;
+function sumatoria(n, C, k) {
+  let result = 0;
 
-    for(let j = 1; j <= n; j++){
-        result += C /(Math.pow(1+k, j));
-    }
-    return result;
+  for (let j = 1; j <= n; j++) {
+    result += C / Math.pow(1 + k, j);
+  }
+  return result;
 }
 
-function ecu(tTentativa, vContado, nCuotas, vCuota){
+function ecu(tTentativa, vContado, nCuotas, vCuota) {
+  let result = -vContado + sumatoria(nCuotas, vCuota, tTentativa);
 
-    let result = -vContado + sumatoria(nCuotas, vCuota, tTentativa);
-
-    return result;
+  return result;
 }
 
-function conversion(tTentativa){
-    
-    //let result = Math.pow((1 + tTentativa), 12) - 1;
+function conversion(tTentativa) {
+  //let result = Math.pow((1 + tTentativa), 12) - 1;
 
-    //return result * 100;
-    return (tTentativa * 1200).toFixed(2);
+  //return result * 100;
+  return (tTentativa * 1200).toFixed(2);
 }
 export const VAN = (vContado, nCuotas, vCuota) => {
+  try {
+    let delta = 5;
+    let tMenor = 0;
+    let tTentativa = 0.1;
+    let tMayor = 0.2;
 
-    try{
+    for (let i = 0; i < 25; i++) {
+      let incTasa = (tMayor - tMenor) / 2;
+      tTentativa = tMenor + incTasa;
+      let ecuacion = ecu(tTentativa, vContado, nCuotas, vCuota);
 
-        let delta = 5;
-        let tMenor = 0;
-        let tTentativa = 0.1;
-        let tMayor = 0.2;
-        
-        for(let i = 0; i < 25; i++){
-            let incTasa = (tMayor - tMenor) / 2;
-            tTentativa = tMenor + incTasa;
-            let ecuacion = ecu(tTentativa, vContado, nCuotas, vCuota);
-            
-            if(ecuacion < delta && ecuacion > 0){
-    
-                return conversion(tTentativa);
-    
-            }else if(ecuacion > 0){
-                
-                tMenor = tTentativa;
-    
-            }else if(ecuacion < 0){
-                
-                tMayor = tTentativa;
-    
-            }
-        }
-        cambiarHistorial(Value, C, n, (Math.pow(1 + k, 12) - 1).toFixed(2));
+      if (ecuacion < delta && ecuacion > 0) {
         return conversion(tTentativa);
-
-    }catch(err){
-
-        throw err;
-
+      } else if (ecuacion > 0) {
+        tMenor = tTentativa;
+      } else if (ecuacion < 0) {
+        tMayor = tTentativa;
+      }
     }
-}
+    cambiarHistorial(vContado, vCuota, nCuotas, conversion(tTentativa));
+    return conversion(tTentativa);
+  } catch (err) {
+    throw err;
+  }
+};
 
 const cambiarHistorial = (Credito, cuota, ncuota, cae) => {
+  db.count({}, function (err, docs) {
+    let nuevaEntrada = {
+      id: docs.toString(),
+      Credito: Credito,
+      Cuota: cuota,
+      nCuotas: ncuota,
+      CAE: cae,
+    };
 
-    db.count({}, function(err, docs){
-
-
-
-        let nuevaEntrada = {
-
-            id: docs.toString(),
-            Credito: Credito,
-            Cuota: cuota,
-            nCuotas: ncuota,
-            CAE: cae,
-    
-        }
-    
-        db.insert(nuevaEntrada);
-
-
-    })
-
-}
+    db.insert(nuevaEntrada);
+  });
+};
--- screens/Historial.js
+++ screens/Historial.js
@@ -7,12 +7,12 @@ import {
   Text,
   StatusBar,
   RefreshControl,
+  Button,
 } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import firebase from '../history/firebase';
-import {db} from '../calculoCae';
+import { db } from "../calculoCae2";
 
-let datos = [{id: "-1"}];
+let datos = [{ id: "-1" }];
 
 const Stack = createStackNavigator();
 //fetch items
@@ -27,18 +27,26 @@ const Item = ({ Cuota, Credito, nCuotas, CAE }) => (
 //Display de items
 function display() {
   const renderItem = ({ item }) => {
-
-    if(item.Cuota === undefined || item.Credito === undefined || item.nCuotas === undefined && item.CAE === undefined) return (<></>);
+    if (
+      item.Cuota === undefined ||
+      item.Credito === undefined ||
+      (item.nCuotas === undefined && item.CAE === undefined)
+    )
+      return <></>;
 
     return (
-    <Item
-      Cuota={'$' + item.Cuota?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
-      Credito={'$' + item.Credito?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
-      nCuotas={item.nCuotas}
-      CAE={item.CAE}
-    />
-  );
-    }
+      <Item
+        Cuota={
+          "$" + item.Cuota?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        }
+        Credito={
+          "$" + item.Credito?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        }
+        nCuotas={item.nCuotas}
+        CAE={item.CAE}
+      />
+    );
+  };
 
   //Refresh (todavia no hace nada)
   const wait = (timeout) => {
@@ -49,12 +57,20 @@ function display() {
 
   const OnRefresh = React.useCallback(() => {
     setRefreshing(true);
+    datos = [{ id: "-1" }];
+    db.find({}).exec(function (err, docs) {
+      docs.forEach((element) =>
+        datos.push(JSON.parse(JSON.stringify(element)))
+      );
+    });
+
+    console.log(db.find({}));
+
     wait(2000).then(() => {
       setRefreshing(false);
     });
   }, []);
 
-  
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -65,15 +81,14 @@ function display() {
           <RefreshControl refreshing={refreshing} onRefresh={OnRefresh} />
         }
       />
+      <Button onPress={() => OnRefresh} title="Refrescar" color="#444" />
     </SafeAreaView>
   );
 }
 
 function Historial() {
-  db.find({}).exec(function(err, docs){
-
-    docs.forEach(element => datos.push(JSON.parse(JSON.stringify(element))));
-
+  db.find({}).exec(function (err, docs) {
+    docs.forEach((element) => datos.push(JSON.parse(JSON.stringify(element))));
   });
   return (
     <Stack.Navigator>
